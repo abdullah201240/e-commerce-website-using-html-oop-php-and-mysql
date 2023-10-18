e566fca6dae9d00fbf8d60dbf39d53ab6401726c
Abdullah Al Sakib <101337497+abdullah201240@users.noreply.github.com>
2023-10-19 04:18:36 +0600

    Add files via upload

---
+++ adminauth.php
@@ -0,0 +1,31 @@
+<?php
+
+use ecom\Controllers\ProductController;
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+include './vendor/autoload.php';
+
+
+
+$userController = new ProductController;
+
+$username = $_POST['username']; 
+$password = $_POST['password'];
+
+$user = $userController->adminlogin($username, $password);
+
+
+if ($user !== false) { 
+ 
+    
+    
+
+    
+    header('location:./views/adminhome.php');
+    
+} else {
+    echo "Authentication failed";
+}
+?>
---
+++ auth.php
@@ -0,0 +1,31 @@
+<?php
+
+use ecom\Controllers\ProductController;
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+include './vendor/autoload.php';
+
+
+
+$userController = new ProductController;
+
+$username = $_POST['username']; 
+$password = $_POST['password'];
+
+$user = $userController->userlogin($username, $password);
+
+
+if ($user !== false) { 
+ 
+    
+    
+
+    
+    header('location:./views/home.php');
+    
+} else {
+    echo "Authentication failed";
+}
+?>
